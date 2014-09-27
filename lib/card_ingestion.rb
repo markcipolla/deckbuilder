7bbe7a0e52876e1b4460d0bd64cab6188e268580
Mark Cipolla <mark@markcipolla.com>
2014-09-27 13:46:00 +1000

    Try this to ingest card numbers

--- lib/card_ingestion.rb
+++ lib/card_ingestion.rb
@@ -33,7 +33,7 @@ class CardIngestion
     @pack = results["pack"]
     @description = results["description"]
     @features = results["features"].collect{ |f| f.split(": ") }
-    @image_id = /-(\d{5})/.match(results["image_id"])[1]
+    @image_id = /-(\d{5})/.match(results["image_id"]).try(:first)
 
     check_types_existence_and_create_if_need_be
     check_packs_existence_and_create_if_need_be
